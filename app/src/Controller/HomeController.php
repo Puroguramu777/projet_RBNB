4ed20bfc24b85bc85424b8fcd33c13dbd1dbb874
Enzo Carrieres <yanniscarrieres@gmail.com>
2024-01-22 10:11:46 +0100

    ajout de controller et de la premiere vue

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route("/home", name:"accueil", methods:['POST', 'GET'])]
+    public function home(){
+
+        return $this->render("home/home.html.twig");
+    }
+
+
+}
